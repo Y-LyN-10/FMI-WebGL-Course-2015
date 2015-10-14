f7a24fe25bd6955484f4de9d78dd80480d49a2cb
Yulia Tenincheva <yulia.ten@dezigner.ru>
2015-10-14 13:04:34 +0300

    My homework is done

--- 01-Hello-WebGL/[HW]-01-Fix-me-please/webgl-fmi.js
+++ 01-Hello-WebGL/[HW]-01-Fix-me-please/webgl-fmi.js
@@ -1,69 +1,72 @@
-﻿
-var gl;		// глобален WebGL контекст
+﻿var gl;		// глобален WebGL контекст
 var glprog;	// глобална GLSL програма
 
+var engine = (function() {
+	'use strict';
 
-// връща WebGL контекст, свързан с HTML елемент с даден id
-function getContext(id)
-{
-	var canvas = document.getElementById(id);
-	if (!canvas)
-	{
-		alert("Искаме canvas с id=\""+id+"\", а няма!");
-		return null;
-	}
-	
-	var context = canvas.getContext("webgl");
-	if (!context)
-	{
-		context = canvas.getContext("experimental-webgl");
-	}
-	
-	if (!context)
-	{
-		alert("Искаме WebGL контекст, а няма!");
-	}
-	
-	return context;
-}
+	// Sorry, but the given code doesn't follow JS quality code conventions
+	// I use my own getContext function from Problem 2, solved in class
+	function getContext(id) {
+		var canvas = document.getElementById(id);
 
-// връща компилиран шейдър
-function getShader(id,type)
-{
-	var source = document.getElementById(id).text;
-	var shader = gl.createShader(type);
+		if (!canvas) {
+			alert('Sorry, canvas with id ' + id + ' is not found');
+			return null;
+		} else {
+			var context = canvas.getContext("webgl");
 
-	gl.shaderSource(shader,source);
-	gl.compileShader(shader);
-	if (!gl.getShaderParameter(shader,gl.COMPILE_STATUS))
-	{
-		alert(gl.getShaderInfoLog(shader));
-		return null;
-	}
-	
-	return shader;
-}
+			if (!context) {
+				context = canvas.getContext("experimental-webgl");
 
+				if (!context) {
+					alert("Sorry, no WebGL context found");
+					return null;
+				}
+			}
 
-// връща готова програма
-function getProgram(idv,idf)
-{
-	var vShader = getShader(idv,gl.VERTEX_SHADER);
-	var fShader = getShader(idf,gl.FRAGMENT_SHADER);
-			
-	if (!vShader || !fShader) {return null;}
-	
-	var shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram,vShader);
-	gl.attachShader(shaderProgram,fShader);
-	gl.linkProgram(shaderProgram);
-
-	if (!gl.getProgramParameter(shaderProgram,gl.LINK_STATUS))
-	{
-		alert(gl.getProgramInfoLog(shaderProgram));
-		return null;
+			return context;
+		}
 	}
 
-	gl.useProgram(shaderProgram);
-	return shaderProgram;
-}
+	function getShader(id, type) {
+		var source = document.getElementById(id).text;
+		var shader = gl.createShader(type);
+
+		gl.shaderSource(shader, source);
+		gl.compileShader(shader);
+
+		if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+			alert(gl.getShaderInfoLog(shader));
+			return null;
+		}
+
+		return shader;
+	}
+
+	function run(idv, idf) {
+		var vShader = getShader(idv, gl.VERTEX_SHADER);
+		var fShader = getShader(idf, gl.FRAGMENT_SHADER);
+
+		if (!vShader || !fShader) {
+			return null;
+		}
+
+		var shaderProgram = gl.createProgram();
+		gl.attachShader(shaderProgram, vShader);
+		gl.attachShader(shaderProgram, fShader);
+		gl.linkProgram(shaderProgram);
+
+		if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+			alert(gl.getProgramInfoLog(shaderProgram));
+			return null;
+		}
+
+		gl.useProgram(shaderProgram);
+		return shaderProgram;
+	}
+
+	return {
+		getContext: getContext,
+		run: run
+	};
+}());
